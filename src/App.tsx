1773b66fe5045d272840328a3876986fb28cd061
Zülal Nebin <zllnbn1997@gmail.com>
2024-04-12 16:04:46 +0300

    visualize movies data on table

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import { useEffect } from "react";
 import "./App.css";
 import { useAppDispatch, useAppSelector } from "./store";
 import { fetchMovies } from "./features/moviesSlice";
+import {
+  Box,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 function App() {
   const movies = useAppSelector((state) => state.movies);
@@ -12,15 +23,37 @@ function App() {
   }, []);
 
   return (
-    <div>
-      {movies.data &&
-        movies.data.Search.map((movie, index) => (
-          <div key={movie.imdbID}>
-            {index + 1}. Name: {movie.Title} Date: {movie.Year} IMDb ID:{" "}
-            {movie.imdbID}{" "}
-          </div>
-        ))}
-    </div>
+    <Container>
+      <Box sx={{ my: 4 }}>
+        {movies.data && movies.data.Search && (
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Release Date</TableCell>
+                  <TableCell align="right">IMDb ID</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {movies.data.Search.map((movie) => (
+                  <TableRow
+                    key={movie.imdbID}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {movie.Title}
+                    </TableCell>
+                    <TableCell align="right">{movie.Year}</TableCell>
+                    <TableCell align="right">{movie.imdbID}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        )}
+      </Box>
+    </Container>
   );
 }
 
